9a331685cd19af64bd245f27b97335961d4fee8b
eee12342 <110887130+eee12342@users.noreply.github.com>
2024-11-13 14:16:53 +0800

    Add and configure bootstrap, add basic bootstrap homepage.

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import os
 from dotenv import load_dotenv
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 
 load_dotenv() # load environment variables
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("FLASK_KEY")
+Bootstrap(app)
 
 
 @app.route("/")
