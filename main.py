d870b5a2267acbc116e02e372854a2b191905ed3
eee12342 <110887130+eee12342@users.noreply.github.com>
2024-11-13 21:39:17 +0800

    Setup database to keep track of content.

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import os
 from dotenv import load_dotenv
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Integer, String, Text
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
 load_dotenv() # load environment variables
@@ -9,6 +12,27 @@ load_dotenv() # load environment variables
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("FLASK_KEY")
 Bootstrap(app)
+
+
+# create and initialise database
+class Base(DeclarativeBase):
+    pass
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///content.db'
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+
+class Book(db.Model):
+    __tablename__ = "books"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+
+    # user filled columns
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    description: Mapped[str] = mapped_column(String(250), unique=False, nullable=True)
+    date: Mapped[str] = mapped_column(String(250), nullable=False)
+    author: Mapped[str] = mapped_column(String(250), nullable=False)
 
 
 @app.route("/")
